d0ea7d1b0e484d71de362b93025ab50863e87391
JohnnyonFlame <johnnyonflame@hotmail.com>
2021-10-10 07:11:41 -0300

    Added `inflate_file` routine.

--- platform/common/zip_util.c
+++ platform/common/zip_util.c
@@ -1,4 +1,6 @@
 #include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
 #include <zip.h>
 
 #include "platform.h"
@@ -13,6 +15,46 @@ zip_t *get_current_apk()
 void set_current_apk(zip_t *apk)
 {
     current_apk = apk;
+}
+
+int inflate_file(zip_t *zip, const char *file, const char *dst)
+{
+    zip_stat_t zip_st;
+    if (zip_stat(zip, file, 0, &zip_st) == -1) {
+        return 0;
+    }
+
+    zip_file_t *f = zip_fopen_index(zip, zip_st.index, ZIP_FL_UNCHANGED);
+    if (f == NULL) {
+        return 0;
+    }
+
+    int fd = open(dst, O_WRONLY | O_TRUNC | O_CREAT, 664);
+    if (fd < 0) {
+        goto inflate_file_zip;
+    }
+
+    zip_int64_t br, left = zip_st.size;
+    uint8_t buf[1024] = {};
+    while (left) {
+        int blk = (left > sizeof(buf)) ? sizeof(buf) : left; 
+        if ((br = zip_fread(f, buf, blk)) != blk) {
+            goto inflate_file_fd;
+        }
+
+        if (write(fd, buf, blk) != blk) {
+            goto inflate_file_fd;
+        }
+        left -= blk;
+    }
+
+    return 1;
+inflate_file_fd:
+    close(fd);
+    remove(dst);
+inflate_file_zip:
+    zip_fclose(f);
+    return 0;
 }
 
 int inflate_buf(zip_t *zip, const char *file, ssize_t *inflated_bytes, void **bytes)
